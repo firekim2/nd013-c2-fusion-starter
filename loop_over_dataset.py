9debfa70046ac416cc2ad128eb2611cc5d530eb9
Sungwoo <firekim2@gmail.com>
2025-06-05 14:18:51 +0900

    s3_Ex1 update

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -48,7 +48,7 @@ import misc.params as params
  
 ##################
 ## Set parameters and perform initializations
-model = 'darknet'
+model = 'fpn_resnet'
 sequence = "1"
 
 ## Select Waymo Open Dataset file and frame numbers
@@ -59,7 +59,7 @@ data_filenames = {
 }
 
 data_filename = data_filenames[sequence]
-show_only_frames = [0, 1] # show only frames in interval for debugging
+show_only_frames = [50, 51] # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', data_filename) # adjustable path in case this script is called from another working directory
@@ -85,10 +85,10 @@ camera = None # init camera sensor object
 np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
-exec_data = ['pcl_from_rangeimage']
-exec_detection = ['bev_from_pcl'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+exec_data = ['pcl_from_rangeimage', 'load_image']
+exec_detection = ['bev_from_pcl', 'detect_objects'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 exec_tracking = [] # options are 'perform_tracking'
-exec_visualization = [] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+exec_visualization = ['show_objects_in_bev_labels_in_camera'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0 # set pause time between frames in ms (0 = stop between frames until key is pressed)
 
--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -60,8 +60,37 @@ def load_configs_model(model_name='darknet', configs=None):
     elif model_name == 'fpn_resnet':
         ####### ID_S3_EX1-3 START #######     
         #######
-        print("student task ID_S3_EX1-3")
-
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.arch = 'fpn_resnet'
+        configs.pin_memory = True
+        configs.conf_thresh = 0.5
+        configs.peak_thresh = 0.2
+        configs.input_size = 608
+        configs.hm_size = (152, 152)
+        configs.batch_size = 1
+        configs.down_ratio = 4
+        configs.max_objects = 50
+        configs.K = 50
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_workers = 1
+        configs.num_samples = None
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+       
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
+        
         #######
         ####### ID_S3_EX1-3 END #######     
 
